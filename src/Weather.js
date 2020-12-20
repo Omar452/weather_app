e866c209037e5d5de1fa6c29f50dcc8ed9161b97
Omar <toumiomar452@gmail.com>
2020-12-20 15:36:07 +0000

    Create Weather class and its constructor

---
+++ src/Weather.js
@@ -0,0 +1,7 @@
+class Weather{
+    contructor(){
+        this.apiKey = 'v4EVVooWoFFNObRYXntceqTJMGZHaYMp';
+        this.cityUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    }
+}
