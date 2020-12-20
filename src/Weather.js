5ca4983431d5b7e5d34e008cc212071fbc6cf83a
Omar <toumiomar452@gmail.com>
2020-12-20 17:38:31 +0000

    Create showWeather function in app.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,16 @@
 class Weather{
     contructor(){
-        this.key = 'v4EVVooWoFFNObRYXntceqTJMGZHaYMp';
+        this.key = 'A7qB8l5cN90PWcZMHo9nYGsTG9qtkwIi';
         this.cityUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
         this.weatherUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
     }
     async getCityDetails(city){
-        const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=v4EVVooWoFFNObRYXntceqTJMGZHaYMp&q=${city}`);
+        const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=A7qB8l5cN90PWcZMHo9nYGsTG9qtkwIi&q=${city}`);
         const data = await response.json();
         return data[0];
     }
     async getWeatherDetails(cityKey){
-        const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=v4EVVooWoFFNObRYXntceqTJMGZHaYMp`);
+        const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=A7qB8l5cN90PWcZMHo9nYGsTG9qtkwIi`);
         const data = await response.json();
         return data[0];
     }
--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 const form = document.querySelector('form');
 const weather = new Weather();
 const cityInput = document.querySelector('#city');
+const weatherDiv = document.querySelector('.weather');
+
+const showWeather = (city) => {
+    weather.getDetails(city)
+    .then(data => {
+        const {cityDetails, weatherDetails} = data;
+        const timeImage = weatherDetails.IsDayTime ? 'src/img/day.svg' : 'src/img/night.svg';
+        const html = 
+            `<div class="card">
+                <img class="card-img-top" src="${timeImage}" alt="time image">
+                <div class="bg-white rounded-circle position-relative icon">
+                    <img src="src/img/icons/${weatherDetails.WeatherIcon}.svg" alt="weather icon">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title">${cityDetails.EnglishName},${cityDetails.Country.ID}</h3>
+                    <p class="card-text">${weatherDetails.WeatherText}</p>
+                    <p class="card-text fs-2">${weatherDetails.Temperature.Metric.Value}&#176;C</p>
+                </div>
+            </div>`;
+        weatherDiv.innerHTML = html;        
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const city = cityInput.value.trim();
-    console.log(city);
     if(city.length >= 2){
-        weather.getCityDetails(city)
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
+        showWeather(city);
     }
 });
